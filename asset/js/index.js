d0b9fa2adcd89d617f471a954f80fd007b7cfec1
emTech-projectDevelopment <emtech.projectdevelopment@gmail.com>
2022-01-28 10:16:20 +1100

    Elias updated changed lines

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,21 +1,3 @@
-// Form Inputs
-const form = document.querySelector('#newTask');
-const taskname = document.querySelector('#taskname');
-const description = document.querySelector('#description');
-const assignedto = document.querySelector('#assignedTo');
-const duedate = document.querySelector('#dueDate');
-
-// Form Buttons
-const btnSubmit = document.querySelector('#btnSubmit');
-const btnReset = document.querySelector('#btnReset');
-
-// Error Messages
-const tasknameError = document.querySelector('#tasknameError');
-const descriptionError = document.querySelector('#descriptionError');
-const assignedToError = document.querySelector('#assignedToError');
-const dueDateError = document.querySelector('#dueDateError');
-const optionValue = document.querySelector('#optionValue');
-
 const dateElement = document.querySelector('#showDate');
 let d = new Date()
 let addMonth = 0
@@ -25,6 +7,18 @@ if (d.getMonth() === 0) {
 dateElement.innerText = `Current date: ${d.getDate()} ${addMonth} ${d.getFullYear()}`
 console.log(d)
 
+// Form Inputs
+const form = document.querySelector('#newTask');
+const taskname = document.querySelector('#taskname');
+const description = document.querySelector('#description');
+const assignedto = document.querySelector('#assignedTo');
+const duedate = document.querySelector('#dueDate');
+const optionValue = document.querySelector('#optionValue');
+
+// Error Messages
+const htmlErrorMessage = document.querySelector('#errorMessage');
+
+
 form.addEventListener('submit', (error)=> {
     let errorMessage = [];
     if(taskname.value.length <= 5) {
@@ -32,7 +26,6 @@ form.addEventListener('submit', (error)=> {
     }
     if (errorMessage.length > 0) {
         error.preventDefault();
-        error.stopPropagation();
-        tasknameError.innerHTML = errorMessage.join(', ');
+        htmlErrorMessage.innerHTML = errorMessage.join(', ');
     }
 });
