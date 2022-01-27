b09b3e74e9539d191c9a9e5c99ab013f40414ab3
Jordan <95255802+gollanj@users.noreply.github.com>
2022-01-27 12:47:49 +1030

    Jordan updated more JS selectors
    Added error messages selectors.

--- asset/js/script.js
+++ asset/js/script.js
@@ -8,3 +8,11 @@ let duedate = document.querySelector('#dueDate');
 // Form Buttons
 let btnSubmit = document.querySelector('#btnSubmit');
 let btnReset = document.querySelector('#btnReset');
+
+// Error Messages
+let tasknameError = document.querySelector('#tasknameError');
+let descriptionError = document.querySelector('#descriptionError');
+let assignedToError = document.querySelector('#assignedToError');
+let dueDateError = document.querySelector('#dueDateError');
+let optionValue = document.querySelector('#optionValue');
+
